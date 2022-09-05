c2074f2aeb469ba9333aba1e762b539187890f73
Connor McGarvey <60416133+Connor-McGarvey@users.noreply.github.com>
2022-09-04 20:37:39 -0400

    Update BitRequirements.java
    testing git, deleted some methods for no reason.

--- BitRequirements.java
+++ BitRequirements.java
@@ -19,11 +19,6 @@ public interface BitRequirements {
 
     Bit and(Bit other); // performs and on two bits and returns a new bit set to the result
 
-    Bit or(Bit other); // performs or on two bits and returns a new bit set to the result
-
-    Bit xor(Bit other); // performs xor on two bits and returns a new bit set to the result
-
-    Bit not(); // performs not on the existing bit, returning the result as a new bit
 
     @Override
     String toString(); // returns “t” or “f”
